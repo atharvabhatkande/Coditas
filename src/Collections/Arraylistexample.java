18b369f896adcbdcdd55b4643454394544773f2e
atharvabhatkande <atharvabhatkande16@gmail.com>
2026-02-08 20:02:33 +0530

    Reverted the deleted files back to github

---
+++ src/Collections/Arraylistexample.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.*;
+
+public class Arraylistexample {
+    public static void main(String [] args){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(50);
+
+        list.add(1,30);
+
+        System.out.println(list);
+
+
+
+    }
+}
